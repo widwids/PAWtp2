fd901be836212bd608bf4bf7f8b3c8f46098e099
Rod <rodobros@hotmail.com>
2019-12-11 21:56:16 -0500

    image now appear on hover

--- TP2/js/main.js
+++ TP2/js/main.js
@@ -1,10 +1,12 @@
 window.addEventListener("load", documentLoaded);
 
-function caca() {
-	alert("caca!");
-}
+
 
 function documentLoaded() {
+
+	function caca() {
+		alert("caca!");
+	}
 
 	// objet "tableau de correspondance" entre les libellés des LI et leur fichier image
 	// vous pouvez choisir un autre thème avec d'autres images
@@ -33,6 +35,71 @@ function documentLoaded() {
 	/* =====================================
 	   insérez ici le code JavaScript du TP2
 	   ===================================== */
+	   /*
+	var currentLevel = 0;
+	var htmlContent = "";
+	for (var key in images) {
+		var currentImageLevel = images[key].niveau;
+		if (currentLevel < currentImageLevel) {
+			htmlContent += "<ul>";
+			currentLevel = currentImageLevel;
+		}
+		if (currentLevel > currentImageLevel) {
+			for (var i = 0 ; i < (currentLevel - currentImageLevel) ; ++i) {
+				htmlContent += "</ul>";
+			}
+			currentLevel = currentImageLevel;
+		}
+		htmlContent += "<i><span>" + key + "</span></i><br>";
+	}
+	htmlContent += "</ul>";
+
+	document.getElementById("liste").innerHTML = htmlContent;
+	*/
+	var niveauPrecedent = 0;
+    var htmlContent = "";
+    for (var key in images) {
+        var currentImageLevel = images[key].niveau;
+
+    	// cas 1- executer quand le niveau augmente
+ 		if (niveauPrecedent  < currentImageLevel) {
+            htmlContent += "<ul>";
+            niveauPrecedent  = currentImageLevel;
+        }
+
+		// cas 2- executer quand le niveau diminue
+        if (niveauPrecedent  > currentImageLevel) {
+            for (var i = 0 ; i < (niveauPrecedent - currentImageLevel) ; ++i) {
+                htmlContent += "</ul>";
+            }
+            niveauPrecedent = currentImageLevel;
+        }
+        htmlContent += "<i><span>" + key + "</span></i><br>";
+    }
+    htmlContent += "</ul>";
+    
+    document.getElementById("liste").innerHTML = htmlContent;
+
+
+	var spans = document.querySelectorAll("span");
+	for (var i = 0 ; i < spans.length ; ++i) {
+		spans[i].addEventListener("mouseover", function(event) {
+			event.target.classList.add("hoveredSpan");
+			var divAffichage = document.getElementById("affichage");
+			var nomFichierImage = images[event.target.textContent].fichier;
+			if (nomFichierImage.length > 0) {
+				divAffichage.innerHTML = "<img src=images/" + nomFichierImage + ">"
+			} else {
+				divAffichage.innerHTML = "<img src=images/No_image_available.png>"
+			}
+		})
+		spans[i].addEventListener("mouseout", function() {
+			event.target.classList.remove("hoveredSpan");
+			var divAffichage = document.getElementById("affichage");
+			divAffichage.innerHTML = "";
+		})
+	}
+
 	
 	
 	// vous pouvez utiliser cette fonction de trace
